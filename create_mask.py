5e0de02615a57fdc5e07e91bfc97c378eb6edb85
Mattzh0 <zhang.matthew@gmail.com>
2024-08-02 17:40:56 -0400

    mask update

--- create_mask.py
+++ create_mask.py
@@ -15,18 +15,18 @@ transforms = A.Compose([
     ToTensorV2()
 ])
 
-if __name__ == '__main__':
-    model = smp.DeepLabV3Plus(encoder_name="resnet101", encoder_weights="imagenet", classes=59)
-    model_path = r'models\trained_deeplabv3plus_model.pth'
-    model.load_state_dict(torch.load(model_path))
+model_path = r'models\trained_deeplabv3plus_model.pth'
+model = smp.DeepLabV3Plus(encoder_name="resnet101", encoder_weights="imagenet", classes=59)
+model.load_state_dict(torch.load(model_path))
 
-    img_dir = dataset.image_directory
-    mask_dir = dataset.mask_directory
+img_dir = dataset.image_directory
+mask_dir = dataset.mask_directory
 
-    test_dataset = dataset.SegmentationDataset(img_dir, dataset.test_img_list, mask_dir, dataset.test_mask_list, transformations=transforms)
-    
+test_dataset = dataset.SegmentationDataset(img_dir, dataset.test_img_list, mask_dir, dataset.test_mask_list, transformations=transforms)
+
+def get_test_sample(test_dataset, index):
     # get a sample from the test dataset
-    sample_image, sample_mask = test_dataset[0]
+    sample_image, sample_mask = test_dataset[index]
 
     # add the batch dimension to the sample image
     sample_image_batch = sample_image.unsqueeze(0)
@@ -41,7 +41,9 @@ if __name__ == '__main__':
     sample_image = sample_image.cpu()
     sample_mask = sample_mask.cpu()
 
-    # plot the images
+    return sample_image, sample_mask, predicted_mask
+
+def plot_images(sample_image, sample_mask, predicted_mask):
     fig, ax = plt.subplots(1, 3, figsize=(15, 5))
 
     ax[0].imshow(sample_image.permute(1, 2, 0))
@@ -55,6 +57,7 @@ if __name__ == '__main__':
 
     plt.show()
 
+def generate_shades(predicted_mask):
     img_array = predicted_mask.numpy()
     shades = np.unique(img_array)
 
@@ -78,3 +81,33 @@ if __name__ == '__main__':
     for i, img in enumerate(shaded_images):
         img_path = os.path.join(output_dir, f'shade_{i}.png')
         img.save(img_path)
+
+def invert_mask_zero():
+    mask_zero_path = r'shades\shade_0.png'
+    img = Image.open(mask_zero_path)
+    img_array = np.array(Image.open(mask_zero_path))
+
+    # create image with the same dimensions as the mask
+    height, width, _ = img_array.shape
+    inverted_image = Image.new('RGBA', (width, height), (0, 0, 0, 0))
+    inverted_array = np.array(inverted_image)
+
+    # invert the mask (previously transparent becomes black, previously black becomes transparent)
+    # img_array[:, :, 3] refers to the alpha channel (transparency)
+    inverted_array[img_array[:, :, 3] == 0] = (0, 0, 0, 255)
+    inverted_array[img_array[:, :, 3] == 255] = (0, 0, 0, 0)
+
+    # convert the array back to an image and save it
+    inverted_image = Image.fromarray(inverted_array, 'RGBA')
+
+    img_path = os.path.join('inverted_mask', 'inverted_mask_zero.png')
+    inverted_image.save(img_path)
+
+def apply_original_colors():
+    pass
+
+if __name__ == '__main__':
+    sample_image, sample_mask, predicted_mask = get_test_sample(test_dataset, 3)
+    generate_shades(predicted_mask)
+    plot_images(sample_image, sample_mask, predicted_mask)
+    invert_mask_zero()
